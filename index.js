c67c201658a524a53360676004ac607e778b0264
Sean Martin <sean@seanmartinstudio.com>
2021-12-12 12:43:58 -0800

    commit tree d175cf085f3ff7a7da12327484f5c332a8743803

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+//1
+
+document.querySelector('main').remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = 'victory'
+
+newHeader.textContent = 'sean martin is the champion'
+
+
+
+
+
+
+
+
